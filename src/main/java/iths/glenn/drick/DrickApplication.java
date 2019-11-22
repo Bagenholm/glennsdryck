f1fc08438239336d2fbd611cb146ca8c1e3b2add
Patrik <bagenholm.patrik@hotmail.com>
2019-11-22 11:53:20 +0100

    Initial commit

---
+++ src/main/java/iths/glenn/drick/DrickApplication.java
@@ -0,0 +1,13 @@
+package iths.glenn.drick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iths/glenn/drick/DrickApplicationTests.java
@@ -0,0 +1,13 @@
+package iths.glenn.drick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
