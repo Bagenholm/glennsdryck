894fb5412387d95c043d791f81ed1af41ec23974
Simon Seabird <m13simon@gmail.com>
2020-01-15 14:00:21 +0000

    UserStorage hotfixed

--- src/main/java/iths/glenn/drick/repository/UserStorage.java
+++ src/main/java/iths/glenn/drick/repository/UserStorage.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface UserStorage extends JpaRepository<DrinkEntity, String> {
+public interface UserStorage extends JpaRepository<UserEntity, String> {
 
     /*@Query(value = "SELECT * FROM drinks WHERE productname IN (SELECT productname FROM drinks GROUP BY productname HAVING count(*) > 1)", nativeQuery = true))
     List<DrinkEntity> findAllSharedDrinks(); */
