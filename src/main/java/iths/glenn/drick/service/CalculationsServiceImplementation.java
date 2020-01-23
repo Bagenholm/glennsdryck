cd2e27aa2e317df422559f0f7c48881b5ac2d2bc
Simon Seabird <m13simon@gmail.com>
2020-01-23 22:09:25 +0000

    Result now considers drunks

--- src/main/java/iths/glenn/drick/service/CalculationsServiceImplementation.java
+++ src/main/java/iths/glenn/drick/service/CalculationsServiceImplementation.java
@@ -22,16 +22,6 @@ public class CalculationsServiceImplementation implements CalculationsService {
 
     static final int FUEL_PRICE = 16;
 
-    /*
-    public int amountOfDrunksForPrice(DrinkEntity drink, double price, double userWeight){
-        int drunks = 0;
-
-        while(price > priceToGetDrunk(drink, 1, userWeight)){
-            price -= priceToGetDrunk(drink, 1, userWeight);
-            drunks++;
-        }
-        return drunks;
-    }*/
 
     @Override
     public List<ResultEntity> priceForDrunks(String username, int drunks, int fetchAmount) {
@@ -39,9 +29,12 @@ public class CalculationsServiceImplementation implements CalculationsService {
         List<DrinkEntity> drinkList = drinksService.findAmountBestApkFromEachStore(fetchAmount);
         List<ResultEntity> resultList = new ArrayList<>();
 
-        drinkList.forEach(drink -> {
-            resultList.add(makeResult(user, drink));
-        });
+        for(DrinkEntity drink : drinkList){
+            ResultEntity result = makeResult(user, drink);
+            result.setTotalDrunks(drunks);
+            result.setTotalPrice(result.getTotalPrice() * drunks);
+            resultList.add(result);
+        }
 
         return resultList;
     }
@@ -69,8 +62,6 @@ public class CalculationsServiceImplementation implements CalculationsService {
             result.setTotalPrice(result.getPriceToGetDrunk() * drunks);
             resultList.add(result);
         }
-
-
         return resultList.stream().sorted(Comparator.comparing(ResultEntity::getAmountOfDrunksForPrice, Collections.reverseOrder())).collect(Collectors.toList());
     }
 
@@ -78,7 +69,6 @@ public class CalculationsServiceImplementation implements CalculationsService {
         ResultEntity result = new ResultEntity();
         List<TripEntity> trips = tripStorage.findAll();
         trips = trips.stream().filter(trip -> trip.getCity().equals(drink.getStore().getCity()) ).collect(Collectors.toList()); //Fullösning. Ska hämta direkt från tripStorage, men vill inte. Varför?
-        //Set<TripEntity> trips = tripStorage.findAllByCityContains(drink.getStore().getCity());
 
         double apk = drink.getAlcoholPerPrice(); // ml alcohol per krona
         double alcoholForOnePromille = user.getWeight() * 0.875;
@@ -110,32 +100,4 @@ public class CalculationsServiceImplementation implements CalculationsService {
     public long fuelConsumptionPriceForTrip(double distanceInKilometer, double fuelConsumptionPerMile){
         return (long) (fuelConsumptionPerMile * (distanceInKilometer / 10)) * 2 * FUEL_PRICE;
     }
-    /*
-    Kvinna: Alkohol i g/(kroppsvikten i kg x 60 %) - (0,15 x timmar från intagets början) = promille
-    Man:    Alkohol i g/(kroppsvikten i kg x 70 %) - (0,15 x timmar från intagets början) = promille
-
-    Man:    if(Alkohol i ML = din vikt * 0.875) = 1 promille
-    Kvinna: if(Alkohol i ML = din vikt * 0.75) = 1 promille
-
-    Basic test formula: Gram alkohol / 52 = promille
-
-    ML alkohol * 0.8 = gram alkohol
-
-    Vodka shot = 6 cl, 40%
-    24 ml alkohol / vodka shot.
-    19.2 g alkohol / vodka shot.
-    0.36923076923 promille / vodka shot.
-
-    (mängdmått) Alkohol Per Krona
-
-    APK = ((mängdmått) mängd * alcohol) / pris
-    APK = (alcohol / pris) * (mängdmått) mängd
-    APK = ((mängdmått) mängd / pris *) alcohol
-
-    1 liter dryck, 100 kr, 10%
-    0.10 / 100 = 0.001 liter APK
-
-    1 deciliter dryck, 10 kr, 10%
-    0.10 / 10 kr = 0.01 dciliter APK
-    */
 }
